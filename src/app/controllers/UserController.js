19636796fdcd0dcf564f204bba9120a871d9efc3
Douglas Porto <douglasalexandre7@gmail.com>
2019-07-03 17:41:19 -0300

    Middleware and update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,6 +9,27 @@ class UserController {
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({
+        where: { email },
+      });
+      if (userExist) {
+        return res.status(400).json({ error: 'User already exist!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
