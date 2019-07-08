71a1ac790467267f6ff83108c1d18821ec1a2c75
Douglas Porto <douglasalexandre7@gmail.com>
2019-07-08 20:06:24 -0300

    ProvidersController

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const uploads = multer(multerConfig);
@@ -18,5 +19,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', uploads.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
