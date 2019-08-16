1b5ec0aa742d7866f5b3fc315d8768857ac74978
Douglas Porto <douglasalexandre7@gmail.com>
2019-08-16 06:38:36 -0300

    Styled Auth

--- front/src/pages/SignIn/index.js
+++ front/src/pages/SignIn/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <div>Login</div>;
+  return (
+    <>
+      <img src={logo} alt="Logo" />
+      <form>
+        <input type="email" placeholder="Seu E-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- front/src/pages/SignUp/index.js
+++ front/src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <div>signup</div>;
+  return (
+    <>
+      <img src={logo} alt="Logo" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu E-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Criar COntra</button>
+        <Link to="/">Já tenho Login</Link>
+      </form>
+    </>
+  );
 }
