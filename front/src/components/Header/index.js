b407184620c05caf1785f44788adba33a27ebb6f
Douglas Porto <douglasalexandre7@gmail.com>
2019-08-21 18:35:55 -0300

    Update profile header

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +9,7 @@ import Logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -18,12 +21,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Douglas Porto</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Avatar Douglas Porto"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt="Avatar"
             />
           </Profile>
         </aside>
