5c2de7c32b0c45ac8e06f0b69b51ca9b10568b6f
Douglas Porto <douglasalexandre7@gmail.com>
2019-08-21 07:10:50 -0300

    Ignore uploads

--- front/src/pages/Profile/index.js
+++ front/src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
+
 import { Container } from './styles';
 
 export default function Profile() {
@@ -16,6 +18,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome Completo" />
         <Input type="email" name="email" placeholder="Seu E-mail" />
         <hr />
--- front/src/store/modules/user/sagas.js
+++ front/src/store/modules/user/sagas.js
@@ -5,9 +5,9 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
     const profile = Object.assign(
-      { name, email },
+      { name, email, avatar_id },
       rest.oldPassword ? rest : {}
     );
     const response = yield call(api.put, 'users', profile);
